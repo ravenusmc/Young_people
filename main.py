d9afe3cf7bc5f0115f88442a204a26ddaa7c9518
ravenusmc <mcuddy77@gmail.com>
2016-12-13 10:09:58 -0500

    added main function

---
+++ main.py
@@ -0,0 +1,16 @@
+#importing all libraries which will be used.
+import pandas as pd
+import numpy as np
+
+#This is the main function which will start the program.
+def main():
+    print("\033c")
+    print("------------------------")
+    print("-------Welcome to-------")
+    print("----Young People Info---")
+    print("------------------------")
+    input("Press enter to continue to main menu ")
+    main_menu()
+
+
+main()
