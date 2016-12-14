4a87bef27ff0bb72597005208ef30fbb33e667d2
ravenusmc <mcuddy77@gmail.com>
2016-12-14 11:46:09 -0500

    worked on a function finding totals

--- draft.py
+++ draft.py
@@ -1,12 +1,14 @@
 #importing all libraries which will be used.
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('responses.csv')
-
 #print(data.head())
+data = data[data.Gender == "male"]
+count = data.Gender.value_counts()
+value = count.iat[0]
+print(value)
 
-test = data[data.Gender == "female"]
-testTwo = test[test.handed == "right handed"]
-print(testTwo)
+
+#print(data.count(axis=0, level=None, numeric_only=False))
--- main.py
+++ main.py
@@ -15,12 +15,20 @@ def main():
     input("Press enter to continue ")
     sex_selection()
 
+#This function will get the total number of individuals who did the survey
+def get_total(data):
+    count = data.Gender.value_counts()
+    female_count = count.iat[0]
+    male_count = count.iat[1]
+    total_count = female_count + male_count
+    return total_count
 
 #This is the main menu function.
 def sex_selection():
     print("\033c")
     #Pulling in the data from the csv.
     data = pd.read_csv('responses.csv')
+    total = get_total(data)
     print("1. Look at data for males")
     print("2. Look at data for females")
     print("3. Look at data for both males and females")
@@ -31,9 +39,19 @@ def sex_selection():
         option = int(input("What is your choice? "))
     if option == 1:
         data = data[data.Gender == "male"]
+        count = data.Gender.value_counts()
+        male_value = count.iat[0]
+        print('You choose to look at data for males')
+        print('Out of', total, 'people there are', male_value, 'Males.')
+        input("Press enter to continue ")
         geography(data)
     elif option == 2:
         data = data[data.Gender == "female"]
+        count = data.Gender.value_counts()
+        female_value = count.iat[0]
+        print('You choose to look at data for females')
+        print('Out of', total, 'people there are', female_value, 'Males.')
+        input("Press enter to continue ")
         geography(data)
     elif option == 3:
         geography(data)
