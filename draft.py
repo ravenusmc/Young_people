5a053a37e0d4272620df73105fa657f478f18d03
ravenusmc <mcuddy77@gmail.com>
2016-12-23 15:19:28 -0500

    workin on graphing country music

--- draft.py
+++ draft.py
@@ -10,22 +10,28 @@ data = data[data.Gender == "female"]
 data = data[data.geography == "city"]
 data = data[[0]]
 #print(data.head())
-print("The graph will now be displayed")
-input("Press Enter to continue")
+#print("The graph will now be displayed")
+#input("Press Enter to continue")
 count, rating = [], []
+count_value = 0
+#print(data.iat[4,0])
+while count_value < 400:
+    rating_value = data.iat[count_value, 0]
+    rating.append(rating_value)
+    count.append(count_value)
+    count_value += 1
+plt.plot(count, rating, linewidth=2)
+plt.title("Country Music", fontsize=24)
+plt.xlabel("Count", fontsize=14)
+plt.ylabel("Rating", fontsize=12)
+plt.show()
 
 
-#while count < 200:
-    #test = ufo[ufo.Time.str.contains(value)]
-    #date.append(value)
-    #number = test.City.count()
-    #year.append(int(number))
-    #newValue = int(value)
-    #newValue += 1
-    #value = str(newValue)
-    #count += 1
-#plt.plot(count, rating, linewidth=2)
-#plt.title("Country Music", fontsize=24)
-#plt.xlabel("Count", fontsize=14)
-#plt.ylabel("Rating", fontsize=12)
-#plt.show()
+#test = ufo[ufo.Time.str.contains(value)]
+#date.append(value)
+#number = test.City.count()
+#year.append(int(number))
+#newValue = int(value)
+#newValue += 1
+#value = str(newValue)
+#count += 1
