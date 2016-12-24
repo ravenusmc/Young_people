ecc2483473a8a380e510bb19e0229df13adcf28f
ravenusmc <mcuddy77@gmail.com>
2016-12-24 11:28:11 -0500

    finished graph function in music file

--- draft.py
+++ draft.py
@@ -9,24 +9,25 @@ data = pd.read_csv('responses.csv')
 data = data[data.Gender == "female"]
 data = data[data.geography == "city"]
 data = data[[0]]
-print(data)
-print(data.mean())
+# print(data)
+# print(data.mean())
+print(len(data))
 #print(data.head())
 #print("The graph will now be displayed")
 #input("Press Enter to continue")
 count, rating = [], []
 count_value = 0
 #print(data.iat[4,0])
-while count_value < 400:
+while count_value < len(data):
     rating_value = data.iat[count_value, 0]
     rating.append(rating_value)
     count.append(count_value)
     count_value += 1
-# plt.plot(count, rating, linewidth=2)
-# plt.title("Country Music", fontsize=24)
-# plt.xlabel("Count", fontsize=14)
-# plt.ylabel("Rating", fontsize=12)
-# plt.show()
+plt.plot(count, rating, linewidth=2)
+plt.title("Country Music", fontsize=24)
+plt.xlabel("Count", fontsize=14)
+plt.ylabel("Rating", fontsize=12)
+plt.show()
 
 
 #test = ufo[ufo.Time.str.contains(value)]
--- music.py
+++ music.py
@@ -1,4 +1,8 @@
 #This file will contain the functions dealing with users who want to look at music
+#importing all libraries which will be used.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Importing files for use
 from valid import *
@@ -25,24 +29,20 @@ def music(data):
         data = data[[0]]
         graph(data)
 
-
 #This function will graph the data.
 def graph(data):
     print("\033c")
     print("The graph will now be displayed")
-    input("Press Enter to continue")
+    input("Press Enter to continue ")
     count, rating = [], []
-    while count < 200:
-      test = ufo[ufo.Time.str.contains(value)]
-      date.append(value)
-      number = test.City.count()
-      year.append(int(number))
-      newValue = int(value)
-      newValue += 1
-      value = str(newValue)
-      count += 1
-    plt.plot(date, year, linewidth=2)
-    plt.title("UFO Sightings By Year", fontsize=24)
-    plt.xlabel("Year", fontsize=14)
-    plt.ylabel("Count", fontsize=12)
+    count_value = 0
+    while count_value < len(data):
+        rating_value = data.iat[count_value, 0]
+        rating.append(rating_value)
+        count.append(count_value)
+        count_value += 1
+    plt.plot(count, rating, linewidth=2)
+    plt.title("Country Music", fontsize=24)
+    plt.xlabel("Count", fontsize=14)
+    plt.ylabel("Rating", fontsize=12)
     plt.show()
